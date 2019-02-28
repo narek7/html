3f738ca3e9c53b411014850eab7f51f1721db558
http://www/Narek Mardoyan <narek.mardoyan.grigori@gmail.com>
2019-02-28 19:26:04 +0400

    Add files via upload

---
+++ HelpWithWeb/HelpWithWeb.js
@@ -0,0 +1,12 @@
+
+var hiddenDiv= document.getElementById("showAnswer");
+hiddenDiv.style.display = "none";
+var answer = document.getElementById("answer");
+
+var showAnswer=function(){
+    var password = document.getElementById("password").value;
+    answer.innerHTML = password;
+    document.getElementById("questionDiv").style.display = "none";
+    hiddenDiv.style.display="block";
+};	
+//This code was wrote Narek Mardoyan!\\
---
+++ HelpWithWeb/HelpWithWeb2.js
@@ -0,0 +1,6 @@
+var myNum1 = 7; 
+var myNum2 = 10; 
+if (myNum1 <myNum2) { 
+   alert ("This sait want to play JavaScript."); 
+}
+//This code was wrote Narek Mardoyan!\\
