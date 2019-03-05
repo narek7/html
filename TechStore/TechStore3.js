1177974f1beaa3697e2da3b89a8081823d827463
http://www/Narek Mardoyan <narek.mardoyan.grigori@gmail.com>
2019-03-05 22:28:02 +0400

    Add files via upload

---
+++ TechStore/TechStore3.js
@@ -0,0 +1,13 @@
+var user = prompt("Please enter your name");
+x=100
+y=100
+alert("Hello!\n"+ user);
+if(x=y){
+	alert("This Site for you!\n"+ user);
+}
+var result = confirm ("Do you true say your name ?")
+if(result==false)
+	alert("You shithead !")
+else{
+	alert("Thank You !");
+}
